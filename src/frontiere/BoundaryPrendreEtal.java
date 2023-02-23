84c9848b91f6778bf582f116016de3d665776cf9
SilasTeuliere <114508097+SilasTeuliere@users.noreply.github.com>
2023-02-23 19:42:55 +0100

    Update BoundaryPrendreEtal.java

--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -13,10 +13,31 @@ public class BoundaryPrendreEtal {
 	}
 
 	public void prendreEtal(String nomVendeur) {
-		//TODO a completer
+		if(!controlPrendreEtal.verifierIdentite(nomVendeur)) {
+			System.out.println("Vous pas vendre ici\n");
+		}
+		else {
+			System.out.println("Bonjour je vais regarder si je trouve une etale\n");
+			if(!controlPrendreEtal.resteEtals()) {
+				installerVendeur(nomVendeur);
+			}
+		}
 	}
 
 	private void installerVendeur(String nomVendeur) {
-		//TODO a completer
+		StringBuilder StrVendeur = new StringBuilder();
+		StrVendeur.append("C'est parfait; etal pour vous\n");
+		StrVendeur.append("Qu'elle produit vous souhaitez vendre?\n");
+		String produit = scan.nextLine();
+		System.out.println("Qu'elle produit vous souhaitez vendre?\n");
+		int nbProduit = scan.nextInt();
+		int numEtal = controlPrendreEtal.prendreEtal(nomVendeur, produit, nbProduit);
+		if(numEtal != 1) {
+			StringBuilder finalStr = new StringBuilder();
+			finalStr.append("Le vendeur "+ nomVendeur);
+			finalStr.append(" s'est intallé à l'etal n°"+ numEtal);
+			finalStr.append("\n");
+			finalStr.toString();
+		}
 	}
 }
